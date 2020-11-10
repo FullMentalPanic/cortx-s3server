6f2be2fad874b8dbfde46af2ef2baafa5a70fe37
Amit Kumar <amit.kumar@seagate.com>
2020-11-10 05:44:00 -0700

    Motr patch fix
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -4,6 +4,7 @@
 # branch: master commit: f67c69f0da129c70b761915f45156ac6af355928
 
 cd motr
+git checkout objopsync
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 ./scripts/m0 rebuild
